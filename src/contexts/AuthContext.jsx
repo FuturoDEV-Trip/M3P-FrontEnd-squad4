91e8f9276753a71dc6b892f46a776b82fc9beb48
Keeity Braga Collodel <keeity@hotmail.com>
2024-10-09 08:12:07 -0300

    adicionado ajuste no front para possibilitar chamada da rota logout se usuario esta logado ou nao

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,6 +1,5 @@
 import { createContext, useState } from "react";
 import PropTypes from "prop-types";
-// import { api } from "../services/api";
 import axios from 'axios';
 
 export const AuthContext = createContext({
@@ -29,37 +28,31 @@ export function AuthProvider({ children }) {
       if(response.status === 200){
         const { id, email, nome } = response.data.user;
         const userData = { id, email, nome };
-        //configura localstorage
+      
               localStorage.setItem('token', response.data.Token)
-              localStorage.setItem('user',JSON.stringify(userData)) //tem que transformar em json. Revisar se salva no localstorage o user no back
+              localStorage.setItem('user',JSON.stringify(userData)) 
          setUser(userData);
        
         return true;
             }
-      // const response = await api(`/users?email=${email}`);
-      // const data = await response.json();
-
-      // if (data.length > 0) {
-      //   const usuario = data[0];
-
-      //   if (usuario.password === password) {
-      //     setUser(usuario);
-      //     localStorage.setItem("@tripflow:user", JSON.stringify(usuario));
-      //     return true;
-      //   }
-      // }
+   
     } catch (error) {
       console.error("Erro ao autenticar", error);
-      return false; // Retorna falha
+      return false; 
     }
 
   }
 
-  function signOut() {
-    setUser(null);
+  async function signOut() {
+  try {
+const userId = user.id;
+ await axios.post("http://localhost:3000/usuario/logout", { userId });
+   setUser(null);
     localStorage.removeItem("token");
     localStorage.removeItem("user");
-  }
+  } catch (error) {
+    console.error("Erro ao deslogar", error);
+  }}
 
   return (
     <AuthContext.Provider value={{ user, signIn, signOut }}>
